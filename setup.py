d6148d7a32489518d5da2c7c6fb3f21621400171
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-07-25 22:08:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
     version='0.1.0',
     description='A Python library for downloading images from Pinterest',
     author='Amal nath',
-    author_email='your_email@example.com',
+    author_email='amalnath0600@gmail.com',
+    url="https://github.com/MrTG-CodeBot",
+    telegram_url="https://t.me/MrTG_Coder",
     packages=find_packages(),
     install_requires=['requests', 'bs4'],
 )
