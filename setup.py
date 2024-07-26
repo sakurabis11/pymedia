d6597aed31c2b995572f02ed3f91afc5b00e262a
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-07-27 00:05:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
-from setuptools import setup, find_packages
+import pathlib
 
-setup(
-    name='pinterest_image_downloader',
-    version='0.1.0',
-    description='A Python library for downloading images from Pinterest',
-    author='Amal nath',
-    author_email='amalnath0600@gmail.com',
-    url="https://github.com/MrTG-CodeBot",
-    telegram_url="https://t.me/MrTG_Coder",
-    packages=find_packages(),
-    install_requires=['requests', 'bs4'],
+import setuptools
+
+setuptools.setup(
+    name="py-cdgram",
+    version="0.2.0",
+    description="This Python package helps you download images from Pinterest URLs.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://amalnath.vercel.app",
+    author="Amal Nath H",
+    author_email="amalnath0600@gmail.com",
+    license="GNU GENERAL PUBLIC LICENSE",
+    project_urls={
+        "Documentation": "https://github.com/MrTG-CodeBot/pymedia/blob/main/README.md",
+        "source": "https://github.com/MrTG-CodeBot/pymedia",
+    },
+    python_requires= ">=3.9.0, <3.11.0",
+    install_requires=["requests", "bs4", "google-generativeai"],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["pymedia= pymedia.cli:main"]}
 )
